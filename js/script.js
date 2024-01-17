54820af45d88ec4c1d714d45be18e7a6b69f6edf
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2024-01-17 15:12:14 +0100

    Added function to check, validate and capture input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Create Global Variables
 //unordered list
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 //button with "Guess"
 const guessButton = document.querySelector(".guess");
 //text input
@@ -15,8 +15,11 @@ const remainingNumber = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 //hidden button for "play again"
 const againButton = document.querySelector(".play-again");
+
 //test word
 const word = "magnolia";
+//guess Letters Array
+let guessedLetters = [];
 
 //Write a Function to Add Placeholders for Letters
 const placeholder = function (word) {
@@ -32,7 +35,42 @@ placeholder(word);
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
     const guess = letterInput.value;
-    console.log(guess);
+    // console.log(guess);
     letterInput.value="";
-
+    message.innerText="";
+    const validatedGuess = guessValidator(guess);
+    // console.log(validatedGuess);
+    makeGuess(validatedGuess);
+    
 });
+
+
+const guessValidator = function (guess) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (guess == "") {
+        message.innerText = "Please enter your guess.";
+        
+    }
+    else if (guess.length > 1) {
+        message.innerText = "Please enter only one letter.";
+    }
+    else if (guess != guess.match(acceptedLetter)){
+        message.innerText = `"${guess}" isn't a letter, try again.`;
+    }
+    else  {
+        message.innerHTML = `You guessed "${guess.toUpperCase()}"!`
+        
+        return guess.toUpperCase();
+    }
+    };
+
+const makeGuess = function(letter) {
+    if (guessedLetters.includes(letter)) {
+        message.innerText = `You already guessed that.`
+    } else if (typeof letter !== 'undefined') {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+    }};
+
+
+
